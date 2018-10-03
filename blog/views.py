0f444d4c3acf31981768a1b8f5732b764176e988
shizzgar <steeji.rat@gmail.com>
2018-10-04 02:10:57 +0300

    Added a 'delete post' btm in eddittng menu

--- blog/views.py
+++ blog/views.py
@@ -53,8 +53,19 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=pk)
 
+
 def publish(self):
     self.published_date = timezone.now()
     self.save()
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+
+
 
 
